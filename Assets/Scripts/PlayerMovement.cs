5dca9d864bd41a0330e9203b569cc30215a8051a
ceje <cdone245@gmail.com>
2020-06-15 14:45:53 -0500

    added pit tile, adjusted scene layout for grid alignment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 oldPos;
-    public float step;
+    public int step;
     private Rigidbody2D rb;
-    private BoxCollider2D bc;
-    public Collider2D wall;
-    private Sprite moveTarget;
-    bool canMove;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        bc = GetComponent<BoxCollider2D>();
         // currentTile = room
     }
 
     // Update is called once per frame
     void Update()
     {
-        oldPos = rb.position;
         if(Input.GetKeyDown("w")){
             rb.MovePosition(rb.position + Vector2.up * step);
             Debug.Log("moving up");
