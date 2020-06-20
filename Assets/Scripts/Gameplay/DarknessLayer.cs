fd24c1c535669a2bdf16dfb87566b2ca34ab297d
ceje <cdone245@gmail.com>
2020-06-20 17:46:44 -0500

    Add Title Scene

---
+++ Assets/Scripts/Gameplay/DarknessLayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DarknessLayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Tilemap tileMap;
+    public TileBase darkness;
+    void Start()
+    {
+        
+        tileMap.BoxFill(new Vector3Int(0,0,0), darkness, -40,-40,10,10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
