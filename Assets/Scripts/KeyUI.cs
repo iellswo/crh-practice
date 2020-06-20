c325256711c570f63471ecce1dfb8f4ed41d4f75
ceje <cdone245@gmail.com>
2020-06-20 17:11:33 -0500

    add key UI element

---
+++ Assets/Scripts/KeyUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyUI : MonoBehaviour
+{
+    public Sprite UIelement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIelement = GetComponent<SpriteRenderer>().sprite;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Data.keyCount > 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = UIelement;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = null;
+        }
+    }
+}
